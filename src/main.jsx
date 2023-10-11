e501ed3231e3e4f30786f2b0fd95c446fc4a07cd
longnt <longnt.en@gmail.com>
2023-10-12 00:43:43 +0700

    second upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import {
-  unstable_HistoryRouter as HistoryRouter ,
-} from 'react-router-dom'
-import { createBrowserHistory } from 'history';
-import LoginModel from './components/RegisterModal';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import LoginModel from "./components/RegisterModal";
 
 export const history = createBrowserHistory({ window });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <HistoryRouter history={history}>
+    <h1>Web site</h1>
     <App />
   </HistoryRouter>
-)
-
+);
